2421d7ce676d5c787764debe2e2d7eb4d56884ff
EllGHT <52287330+eightroutes@users.noreply.github.com>
2024-10-10 13:03:47 +0900

    feat: 카카오 로그인 테스트, key 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,25 @@ import 'package:cano/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 
-void main() {
-  runApp(ProviderScope(child: MyApp()));
+void main() async {
+  await dotenv.load(fileName: '.env');
+  WidgetsFlutterBinding.ensureInitialized();
+  String? kakaoNativeAppKey = dotenv.env['KAKAO_NATIVE_APP_KEY'];
+  KakaoSdk.init(
+    nativeAppKey: kakaoNativeAppKey,
+  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var _index = 0;
+    var index = 0;
 
     WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
     FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
